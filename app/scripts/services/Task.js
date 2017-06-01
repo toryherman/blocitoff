f3d520cfd3e194bf5b9d066f5e2e4891f6fb8a11
Tory Herman <tory.herman12@gmail.com>
2017-06-01 00:25:22 -0500

    add move task functions

--- app/scripts/services/Task.js
+++ app/scripts/services/Task.js
@@ -5,16 +5,40 @@
     var ref = firebase.database().ref().child("tasks");
     var tasks = $firebaseArray(ref);
 
+    var getIndex = function(id) {
+      for (var i = 0; i < tasks.length; i++) {
+        if (tasks[i].$id == id) {
+          return i;
+        }
+      }
+    }
+
     Task.getTasks = function() {
       return tasks;
     };
 
     Task.addTask = function(item) {
-      tasks.$add(item);
+      tasks.$add({
+        'content': item,
+        'index': tasks.length
+      });
     };
 
-    Task.deleteTask = function(index) {
+    Task.deleteTask = function(id) {
+      var index = getIndex(id);
       tasks.$remove(index);
+    }
+
+    Task.minusIndex = function(id) {
+      var index = getIndex(id);
+      tasks[index].index--;
+      tasks.$save(index);
+    }
+
+    Task.plusIndex = function(id) {
+      var index = getIndex(id);
+      tasks[index].index++;
+      tasks.$save(index);
     }
 
     return Task;
