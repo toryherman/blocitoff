c7a62aac9fb69b07c10a68f60782e2d5b70797ac
Tory Herman <tory.herman12@gmail.com>
2017-06-01 20:50:53 -0500

    update move task functions to alter next or prev element

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -7,26 +7,23 @@
       if (event && event.keyCode !== 13) { return };
       Task.addTask($scope.newTask);
       $scope.newTask = '';
-    }
+    };
 
     $scope.deleteTask = function(element) {
       Task.deleteTask(element.task.$id);
-    }
+    };
 
     $scope.moveUpTask = function(element) {
       if (!element.$first) {
-        console.log(element);
         Task.minusIndex(element.task.$id);
-        Task.plusIndex(element.$$prevSibling.task.$id);
       }
-    }
+    };
 
     $scope.moveDownTask = function(element) {
       if (!element.$last) {
         Task.plusIndex(element.task.$id);
-        Task.plusIndex(element.$$nextSibling.task.$id);
       }
-    }
+    };
   }
 
   angular
--- app/scripts/services/Task.js
+++ app/scripts/services/Task.js
@@ -5,14 +5,29 @@
     var ref = firebase.database().ref().child("tasks");
     var tasks = $firebaseArray(ref);
 
+    // local functions
     var getIndex = function(id) {
       for (var i = 0; i < tasks.length; i++) {
         if (tasks[i].$id == id) {
           return i;
         }
       }
-    }
+    };
 
+    var moveOtherElement = function(index, direction) {
+      var myRank = tasks[index].rank;
+      var myContent = tasks[index].content;
+      for (var i = 0; i < tasks.length; i++) {
+        if ((tasks[i].rank == myRank) && (tasks[i].content != myContent)) {
+          direction == 'up' ? tasks[i].rank++ : tasks[i].rank--;
+          tasks.$save(i);
+          break;
+        }
+      }
+    };
+
+
+    // global functions
     Task.getTasks = function() {
       return tasks;
     };
@@ -20,7 +35,7 @@
     Task.addTask = function(item) {
       tasks.$add({
         'content': item,
-        'index': tasks.length
+        'rank': tasks.length
       });
     };
 
@@ -31,14 +46,16 @@
 
     Task.minusIndex = function(id) {
       var index = getIndex(id);
-      tasks[index].index--;
+      tasks[index].rank--;
       tasks.$save(index);
+      moveOtherElement(index, 'up');
     }
 
     Task.plusIndex = function(id) {
       var index = getIndex(id);
-      tasks[index].index++;
+      tasks[index].rank++;
       tasks.$save(index);
+      moveOtherElement(index, 'down');
     }
 
     return Task;
